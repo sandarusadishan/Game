6ed76aba8d6635a319c57d3dc8a2ecfedfa55e73
sandarusadishan <sandarusadishan0404@gmail.com>
2024-10-30 11:23:25 +0530

    neww

--- app.js
+++ app.js
@@ -12,15 +12,12 @@ document.getElementById("submitguess").onclick = function () {
     );
   } else if (attempts >= 3) {
     alert(
-      "SORRY! You have used all 3 attempts. The correct number was " + y + "."
-    );
+      "SORRY! You have used all 3 attempts. The correct number was " + y + ".");
   } else if (x > y) {
     alert(
-      " SORRY! TRY A SMALLER NUMBER. Attempts left: " + (3 - attempts)
-    );
+      " SORRY! TRY A SMALLER NUMBER. Attempts left: " + (3 - attempts));
   } else {
     alert(
-      " SORRY! TRY A GREATER NUMBER. Attempts left: " + (3 - attempts)
-    );
+      " SORRY! TRY A GREATER NUMBER. Attempts left: " + (3 - attempts));
   }
 };
